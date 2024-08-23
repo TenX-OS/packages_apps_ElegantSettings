cae176a36246d58d39ce8fa2c7d1b5a0041867eb
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-23 14:50:48 +0530

    TenX: Add support for json-based (PIF) spoofing [2/2]
    Co-Authored-By: drkphnx <dark.phnx12@gmail.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- src/com/tenx/settings/fragments/Misc.java
+++ src/com/tenx/settings/fragments/Misc.java
@@ -19,17 +19,28 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.SystemProperties;
 import android.provider.Settings;
+import android.util.Log;
+
 import androidx.preference.*;
 
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.tenx.SystemRestartUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.tenx.support.preferences.SystemSettingSwitchPreference;
 import com.tenx.support.preferences.TenXPreference;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+
+import org.json.JSONObject;
 
 public class Misc extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
@@ -37,13 +48,18 @@ public class Misc extends SettingsPreferenceFragment
     public static final String TAG = "Misc";
 
     private static final String KEY_POCKET_JUDGE = "pocket_judge";
+    private static final String KEY_PIF_JSON_FILE_PREFERENCE = "pif_json_file_preference";
 
     private SystemSettingSwitchPreference mPocketJudge;
+    private Preference mPifJsonFilePreference;
+    private Handler mHandler;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.tenx_settings_misc);
+
+        mHandler = new Handler();
 
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources res = getResources();
@@ -54,6 +70,42 @@ public class Misc extends SettingsPreferenceFragment
                 com.android.internal.R.bool.config_pocketModeSupported);
         if (!mPocketJudgeSupported)
             prefScreen.removePreference(mPocketJudge);
+        mPifJsonFilePreference = findPreference(KEY_PIF_JSON_FILE_PREFERENCE);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference == mPifJsonFilePreference) {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("application/json");
+            startActivityForResult(intent, 10001);
+            return true;
+        }
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 10001 && resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getData();
+            Log.d(TAG, "URI received: " + uri.toString());
+            try (InputStream inputStream = getActivity().getContentResolver().openInputStream(uri)) {
+                if (inputStream != null) {
+                    String json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+                    Log.d(TAG, "JSON data: " + json);
+                    JSONObject jsonObject = new JSONObject(json);
+                    for (Iterator<String> it = jsonObject.keys(); it.hasNext(); ) {
+                        String key = it.next();
+                        String value = jsonObject.getString(key);
+                        Log.d(TAG, "Setting property: persist.sys.pihooks_" + key + " = " + value);
+                        SystemProperties.set("persist.sys.pihooks_" + key, value);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error reading JSON or setting properties", e);
+        }
     }
 
     @Override
