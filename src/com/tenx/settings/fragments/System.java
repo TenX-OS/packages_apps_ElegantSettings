11b8b6160474d81e2a135bd8c656e95235a8741b
shripal17 <Shripal17@gmail.com>
2024-08-25 21:38:34 +0530

    TenX: [SQUASHED] Add Flashlight blink on incoming calls [3/3]
    Co-authored-by: idoybh <idoybh2@gmail.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- src/com/tenx/settings/fragments/System.java
+++ src/com/tenx/settings/fragments/System.java
@@ -16,6 +16,7 @@
 package com.tenx.settings.fragments;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -30,7 +31,12 @@ import com.android.settings.SettingsPreferenceFragment;
 
 import com.tenx.settings.utils.TelephonyUtils;
 
+import com.tenx.support.preferences.SystemSettingListPreference;
+import com.tenx.support.preferences.SystemSettingSwitchPreference;
+import com.tenx.support.preferences.SystemSettingSeekBarPreference;
 import com.tenx.support.preferences.TenXPreferenceCategory;
+
+import com.android.internal.util.tenx.Utils;
 
 public class System extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
@@ -41,6 +47,14 @@ public class System extends SettingsPreferenceFragment
     private static final String KEY_VIBRATE_CONNECT = "vibrate_on_connect";
     private static final String KEY_VIBRATE_CALLWAITING = "vibrate_on_callwaiting";
     private static final String KEY_VIBRATE_DISCONNECT = "vibrate_on_disconnect";
+    private static final String FLASHLIGHT_CATEGORY = "flashlight_category";
+    private static final String FLASHLIGHT_CALL_PREF = "flashlight_on_call";
+    private static final String FLASHLIGHT_DND_PREF = "flashlight_on_call_ignore_dnd";
+    private static final String FLASHLIGHT_RATE_PREF = "flashlight_on_call_rate";
+
+    private SystemSettingListPreference mFlashOnCall;
+    private SystemSettingSwitchPreference mFlashOnCallIgnoreDND;
+    private SystemSettingSeekBarPreference mFlashOnCallRate;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,16 +62,45 @@ public class System extends SettingsPreferenceFragment
         addPreferencesFromResource(R.xml.tenx_settings_system);
 
         final PreferenceScreen prefScreen = getPreferenceScreen();
+        final Context mContext = getActivity().getApplicationContext();
+        final ContentResolver resolver = mContext.getContentResolver();
 
         final PreferenceCategory vibCategory = prefScreen.findPreference(KEY_VIBRATE_CATEGORY);
 
         if (!TelephonyUtils.isVoiceCapable(getActivity())) {
             prefScreen.removePreference(vibCategory);
         }
+
+        if (!Utils.deviceHasFlashlight(mContext)) {
+            final PreferenceCategory flashlightCategory =
+                    (PreferenceCategory) prefScreen.findPreference(FLASHLIGHT_CATEGORY);
+            prefScreen.removePreference(flashlightCategory);
+        } else {
+            mFlashOnCall = (SystemSettingListPreference)
+                    prefScreen.findPreference(FLASHLIGHT_CALL_PREF);
+            mFlashOnCall.setOnPreferenceChangeListener(this);
+
+            mFlashOnCallIgnoreDND = (SystemSettingSwitchPreference)
+                    prefScreen.findPreference(FLASHLIGHT_DND_PREF);
+            int value = Settings.System.getInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL, 0);
+
+            mFlashOnCallRate = (SystemSettingSeekBarPreference)
+                    prefScreen.findPreference(FLASHLIGHT_RATE_PREF);
+
+            mFlashOnCallIgnoreDND.setEnabled(value > 1);
+            mFlashOnCallRate.setEnabled(value > 0);
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mFlashOnCall) {
+            int value = Integer.parseInt((String) newValue);
+            mFlashOnCallIgnoreDND.setEnabled(value > 1);
+            mFlashOnCallRate.setEnabled(value > 0);
+            return true;
+        }
         return false;
     }
 
